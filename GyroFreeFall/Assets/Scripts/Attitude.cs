d9d64475f921146664d5cbde3ee195f5d2d10dc3
roodey145 <abdu2624@outlook.com>
2022-03-31 15:00:53 +0200

    Small optimization and Comment
    Optimizing the code and descripting the purpose of each line code.

--- GyroFreeFall/Assets/Scripts/Attitude.cs
+++ GyroFreeFall/Assets/Scripts/Attitude.cs
@@ -5,79 +5,114 @@ using UnityEngine;
 
 public class Attitude : MonoBehaviour
 {
-    public float movementSpeed = 20;
-    Gyroscope gyro;
+    // Get access to the gyroscope in the global scope
+    private Gyroscope _gyro;
 
-    float MAX_AGNLE = 60;
+    // Flags
+    //-----------------------------------------
     private bool _collectData = false;
     private volatile bool _savingData = false;
-    private int collection = 1;
-    private List<string> _gravity = new List<string>();
+    //-----------------------------------------
+
+    // Current save number
+    private int _collection = 1;
+
+    // A list to save the current record info (X, Y, Z)
+    // A float was used instead of string to save memory space
+    /* Each float is 4 byte, however, each string is 2byte * character.length
+     * This may differ depending on the character encoding */
+    private List<float[]> _gravity = new List<float[]>();
 
     private void Start()
     {
-        Debug.Log(Application.dataPath);
-        gyro = Input.gyro;
+        // Write the path in which the information will be saved in
+        // Debug.Log(Application.dataPath);
 
+        // Get access to the gyroscope
+        _gyro = Input.gyro;
+
+        // Check if the gyroscope supported
         if (SystemInfo.supportsGyroscope)
         {
-            gyro.enabled = true;
-            Debug.LogWarning("Gyro Enabled");
+            // Enable the gyroscope
+            _gyro.enabled = true;
+
+            // Write that the gyro is enabled
+            // Debug.LogWarning("Gyro Enabled");
         }
     }
 
     private void Update()
     {
 
-        /*Vector3 rotation = new Vector3(); ;
-        rotation.x = gyro.gravity.y * MAX_AGNLE;
-        rotation.z = -gyro.gravity.x * MAX_AGNLE;
 
-        Debug.Log("X: " + rotation.x);
-        Debug.Log("Z: " + rotation.z);
-
-        transform.eulerAngles = rotation;*/
+        /* Check if the user started recording the gyro info 
+         * and the last record is not being saved atm */
         if (_collectData && !_savingData)
-            _gravity.Add(gyro.gravity.x + ","
-                           + gyro.gravity.y + ","
-                           + gyro.gravity.z);
-            // Debug.Log(gyro.gravity);
+            // Record the gyro gravity-vector info
+            _gravity.Add(new float[] {_gyro.gravity.x,
+                           _gyro.gravity.y,
+                           _gyro.gravity.z } );
     }
 
 
     public void toggle()
     {
-        // Make sure the button is not clicked while 
-        // The system is saving the data on a file
+        /* Make sure the button is not clicked while 
+         * The system is saving the data on a file */
         if (_savingData)
             return;
+
+        // Reverese the flag value
         _collectData = !_collectData;
 
+        /* The _collectData starts being false, after the first click
+         * it will become true, therefor the if-statement below will not
+         * be executed unless the user clicks twice on the button, to ensure
+         * there is info recorded*/
         if(_collectData == false && _gravity.Count > 0)
         { // There is data to be saved
             SaveData(_gravity);
         }
     }
 
-    public void SaveData(List<string> dataToBeSaved)
+    public void SaveData(List<float[]> dataToBeSaved)
     {
+        // Indicate that the data are being saved
+        // The _saveingData is volatile, to make sure the new value will be
+        // saved to the main memory directly
         _savingData = true;
 
-        Debug.Log("Started: " + dataToBeSaved.Count);
-        StreamWriter sw = new StreamWriter(Application.dataPath + "DataSet" + (collection++) + ".csv", false);
-        // Write the headers
+        // To know the current record number 
+        // Debug.Log("Started: " + dataToBeSaved.Count);
+        
+        // Create a stream writer to start writing to a file
+        /* A post-increment is used on the _collection variable to make sure
+         * the name of the saved files are different */
+        /* Note: There is no slash before the file name (DataSet...csv) which means the file
+         * will not be in the last directoy of the dataPath field but in the folder one level
+         * before it. In other word the files will not be saved in the Assets folder but
+         * in the GyroFreeFall (the parent of the Assets folder) */
+        StreamWriter sw = new StreamWriter(Application.dataPath + "DataSet" + (_collection++) + ".csv", false);
+        // Write the headers on the first line of the file
         sw.WriteLine("X,Y,Z");
 
+        // Loop through all the list float arrays to save their values
         for(int i = 0; i < dataToBeSaved.Count; i++)
         {
-            sw.WriteLine(dataToBeSaved[i]);
+            // Write this element x, y, z and seperate the values with commas
+            sw.WriteLine(dataToBeSaved[i][0] + ", " + dataToBeSaved[i][1] + ", " + dataToBeSaved[i][2]);
         }
 
         // Close the file to make sure the data is written on it
+        // In other word to make sure the buffer is written directly 
+        // On the file in the secondary storage (internal storage, Universal Serial Bus) etc.
         sw.Close();
 
-        // Clear the old data
+        // Clear the old data, to prepare the list to save new record
         dataToBeSaved.Clear();
+
+        // Indicate the end of the record saving process to external file
         _savingData = false;
     }
 }
